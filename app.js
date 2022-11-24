9c0a9396769742b0bdab2339691a168483643c90
Matthew Lowe <Matthewjlowe11@gmail.com>
2022-11-24 12:03:04 -0500

    Use JS server to display code

--- app.js
+++ app.js
@@ -5,7 +5,13 @@ const port = 3000;
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("Hello")
+  var today = new Date();
+  if(today.getDay() === 7 || today.getDay() === 7) // Sat is 7 - Sunday is 0
+  {
+    res.send("No! It's a weekend");
+  } else {
+    res.send("Its a weekend!");
+  }
 })
 
 app.listen(port, () => {
