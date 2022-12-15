b5980683f35fcad7d0c0bf214256fb735dde4ee8
Matthew Lowe <Matthewjlowe11@gmail.com>
2022-12-15 11:06:15 -0500

    Using best date method to get date value

--- app.js
+++ app.js
@@ -7,36 +7,15 @@ const app = express();
 app.set('view engine', 'ejs');
 
 app.get("/", (req, res) => {
-  var today = new Date();
-  var day = today.getDay();
-  let currentDay = "";
+  const today = new Date();
+ 
+  const options = { 
+    weekday: 'long', 
+    month: 'long', 
+    day: 'numeric' 
+  };
 
-  switch(day) {
-    case 0:
-      currentDay = "Sunday";
-    break;
-    case 1:
-      currentDay = "Monday"
-    break;
-    case 2:
-      currentDay = "Tuesday";
-    break;
-    case 3:
-      currentDay = "Wednesday";
-    break;
-    case 4:
-      currentDay = "Thursday";
-    break;
-    case 5:
-      currentDay = "Friday";
-    break;
-    case 6:
-      currentDay = "Saturday";
-    break;
-    default:
-      currentDay = "Cannot find day";
-      console.log("Error: Day value cannot be found");
-  }
+  const currentDay = today.toLocaleDateString("en-US", options);
 
   res.render('lists', 
   {
