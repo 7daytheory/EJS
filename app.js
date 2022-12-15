49e35896917ec14c011b8ff3658d4ae0a10ffd63
Matthew Lowe <Matthewjlowe11@gmail.com>
2022-12-15 11:26:30 -0500

    Sending values to server and displaying new values entered

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const port = 3000;
-
 const app = express();
 
+let item = "";
+let items = [];
+
 app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", (req, res) => {
   const today = new Date();
@@ -17,10 +21,19 @@ app.get("/", (req, res) => {
 
   const currentDay = today.toLocaleDateString("en-US", options);
 
-  res.render('lists', 
+  res.render("list", 
   {
-    currentDay: currentDay
+    currentDay: currentDay,
+    newlistItem: items
   });
+})
+
+app.post("/", (req, res) => {
+  item = req.body.newItem;
+
+  items.push(item);
+
+  res.redirect("/");
 })
 
 app.listen(port, () => {
